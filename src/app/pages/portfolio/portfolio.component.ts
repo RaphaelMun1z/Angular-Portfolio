e128b42f6247c058462ab75129d9d722180facc6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-11-24 19:55:43 -0300

    a

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+    selector: 'app-portfolio',
+    standalone: true,
+    imports: [RippleModule],
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+    
 }
