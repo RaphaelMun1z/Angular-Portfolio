c1e20a7ba368a42e6bd6d2d9af0f6ffb40796cff
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-11-25 00:32:05 -0300

    a

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ServicesComponent } from "../../components/services/services.component";
 import { AboutComponent } from "../../components/about/about.component";
 import { TestimonialsComponent } from "../../components/testimonials/testimonials.component";
@@ -8,6 +8,11 @@ import { ContactComponent } from "../../components/contact/contact.component";
 import { HeroComponent } from "../../components/hero/hero.component";
 import { DropdownComponent } from "../../components/dropdown/dropdown.component";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 @Component({
     selector: 'app-home',
     standalone: true,
@@ -16,5 +21,24 @@ import { DropdownComponent } from "../../components/dropdown/dropdown.component"
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+    constructor(private el: ElementRef) {}
     
+    ngAfterViewInit() {
+        const elementos = this.el.nativeElement.querySelectorAll('.animated');
+        
+        elementos.forEach((elemento: HTMLElement) => {
+            gsap.from(elemento, {
+                opacity: 0,
+                y: 50, // Translação inicial no eixo Y
+                duration: 1,
+                scrollTrigger: {
+                    trigger: elemento,  // Define o elemento como o gatilho da animação
+                    start: 'top 80%',   // Início da animação
+                    end: 'top 20%',     // Fim da animação
+                    scrub: false,       // Animação ocorre suavemente
+                    once: true          // Garante que a animação aconteça apenas uma vez
+                }
+            });
+        });
+    }
 }
