49d816a0d8c48cea8f3f5b061d31b51e686995a6
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-01-12 23:47:15 -0300

    build: deploy project to GitHub Pages

--- src/app/components/vertical-carousel/vertical-carousel.component.ts
+++ src/app/components/vertical-carousel/vertical-carousel.component.ts
@@ -4,13 +4,12 @@ import { register as registerSwiperElements } from 'swiper/element/bundle';
 registerSwiperElements();
 
 @Component({
-  selector: 'app-vertical-carousel',
-  standalone: true,
-  imports: [],
-  templateUrl: './vertical-carousel.component.html',
-  styleUrl: './vertical-carousel.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    selector: 'app-vertical-carousel',
+    standalone: true,
+    imports: [],
+    templateUrl: './vertical-carousel.component.html',
+    styleUrl: './vertical-carousel.component.scss',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class VerticalCarouselComponent {
-
 }
